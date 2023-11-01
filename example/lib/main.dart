4a50fdf8b1e42c7cc0e659c071026877667acb41
thenewsyria <96256695+thenewsyria@users.noreply.github.com>
2023-11-01 12:43:39 +0300

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,70 +1,109 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(DragAndMoveImages());
+  runApp(DragAndMoveWidgets());
 }
 
-class DragAndMoveImages extends StatefulWidget {
+class DragAndMoveWidgets extends StatefulWidget {
   @override
-  _DragAndMoveImagesState createState() => _DragAndMoveImagesState();
+  _DragAndMoveWidgetsState createState() => _DragAndMoveWidgetsState();
 }
 
-class _DragAndMoveImagesState extends State<DragAndMoveImages> {
-  List<Widget> images = [
-    Image.asset('assets/images/image1.png'),
-    Image.asset('assets/images/image2.png'),
-    Image.asset('assets/images/image3.png'),
-  ];
-
-  List<Offset> imagePositions = [
-    Offset(20, 20),
-    Offset(50, 50),
-    Offset(100, 100),
-  ];
-
-  double imageScale = 0.5; // Adjust this value to change the image size.
+class _DragAndMoveWidgetsState extends State<DragAndMoveWidgets> {
+  List<Widget> widgets = [];
+  List<Offset> widgetPositions = [];
+  double widgetScale = 1.0;
 
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Overlay(
-        initialEntries: [
-          OverlayEntry(
-            builder: (context) => MediaQuery(
-              data: MediaQueryData(),
-              child: Scaffold(
-                body: Stack(
-                  children: images.asMap().entries.map((entry) {
-                    int index = entry.key;
-                    return Positioned(
-                      left: imagePositions[index].dx,
-                      top: imagePositions[index].dy,
-                      child: Draggable(
-                        child: Transform.scale(
-                          scale: imageScale,
-                          child: entry
-                              .value, // Wrap the image with Transform.scale
-                        ),
-                        feedback: Container(
-                          color: Colors.blue,
-                          width: 10,
-                          height: 10,
-                        ),
-                        onDragUpdate: (details) {
-                          setState(() {
-                            imagePositions[index] = details.localPosition;
-                          });
-                        },
-                      ),
-                    );
-                  }).toList(),
+    return MaterialApp(
+      // Wrap your app in MaterialApp
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Overlay(
+          initialEntries: [
+            OverlayEntry(
+              builder: (context) => MediaQuery(
+                data: MediaQueryData(),
+                child: Scaffold(
+                  body: Stack(
+                    children: [
+                      ...widgetPositions.asMap().entries.map((entry) {
+                        int index = entry.key;
+                        return Positioned(
+                          left: widgetPositions[index].dx,
+                          top: widgetPositions[index].dy,
+                          child: Draggable(
+                            child: Transform.scale(
+                              scale: widgetScale,
+                              child: widgets[index],
+                            ),
+                            feedback: Container(
+                              width: 100,
+                              height: 100,
+                              color: Colors.blue,
+                            ),
+                            onDragUpdate: (details) {
+                              setState(() {
+                                widgetPositions[index] = details.localPosition;
+                              });
+                            },
+                          ),
+                        );
+                      }),
+                    ],
+                  ),
+                  floatingActionButton: FloatingActionButton(
+                    onPressed: () {
+                      _showAddWidgetDialog(context);
+                    },
+                    child: Icon(Icons.add),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+    );
+  }
+
+  void _showAddWidgetDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text("Add Widget"),
+          children: [
+            SimpleDialogOption(
+              onPressed: () {
+                setState(() {
+                  widgets.add(Text("New Text"));
+                  widgetPositions.add(Offset(20, 20));
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text("Text"),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                setState(() {
+                  widgets.add(
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                  );
+                  widgetPositions.add(Offset(20, 20));
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text("Image"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
